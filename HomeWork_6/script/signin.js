c422c75e26565ecf3f0ce37d20b806146be0e360
IrakliParulava <124196888+IrakliParulava@users.noreply.github.com>
2023-06-13 16:31:36 +0200

    Add files via upload

--- HomeWork_6/script/signin.js
+++ HomeWork_6/script/signin.js
@@ -1,6 +1,6 @@
 function signIn(e) {
     e.preventDefault();
-    let data = {
+    let loginData = {
         "username": document.getElementById("username").value,
         "password": document.getElementById("password").value
     };
@@ -9,21 +9,15 @@ function signIn(e) {
         "headers": {
             "content-type": "application/json",
         },
-        "body": JSON.stringify(data),
+        "body": JSON.stringify(loginData),
         "method": "POST",
     })
-    .then((response) => {
-        if (response.status === 200) {
-            alert("Sign in successful");
-            return response.json();
-        } else if (response.status === 401) {
-            alert("Invalid username or password");
-        } else {
-            alert("Sign in failed");
-        }
-    })
-    .then((data) => {
-        localStorage.setItem("id", data.id);
-        localStorage.setItem("jwt", data.jwt);
-    })
+        .then((response) => {
+            if (response.status === 200) {
+                alert("Sign in successful");
+                return response.json();
+            } else if (response.status === 401) {
+                alert("Invalid username or password");
+            }
+        })
 }
